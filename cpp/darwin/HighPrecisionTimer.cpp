29bfadc1c1dfa1fbd9f1e79b810355eaeec7f1b0
Mingxin Zhang <m.zhang@hapis.u-tokyo.ac.jp>
2022-12-20 02:59:24 +0900

    fixed the problem of gettimeofday overflow

--- cpp/darwin/HighPrecisionTimer.cpp
+++ cpp/darwin/HighPrecisionTimer.cpp
@@ -2,7 +2,7 @@
  * @Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
  * @Date: 2022-12-19 16:32:53
  * @LastEditors: Mingxin Zhang
- * @LastEditTime: 2022-12-19 18:50:07
+ * @LastEditTime: 2022-12-20 02:59:02
  * Copyright (c) 2022 by Mingxin Zhang, All Rights Reserved. 
  */
 
@@ -18,11 +18,12 @@ class HighPrecisionTimer
 float HighPrecisionTimer::HighPrecisionSleep(float deltaTime)
 {
     timeval start, end;
-    auto elapsedTime = 0.0;
+    double elapsedTime = 0.0;  // unit is us
     gettimeofday(&start, NULL);
     while (elapsedTime < deltaTime * 1e6) {
         gettimeofday(&end, NULL);
         elapsedTime = (double)(end.tv_usec - start.tv_usec);
+        if (elapsedTime < 0) break;
     }
     return elapsedTime;
 }
