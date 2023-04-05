4c73e3f1ffc1f2a4b72c7fa1c2cf4d48f292f08d
SUN Qingyu <sunqingyu1997@gmail.com>
2023-04-05 17:35:40 +0900

    Hello!

---
+++ softness_rendering/handtrack_new.py
@@ -0,0 +1,118 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import mediapipe as mp
+import math
+
+# Initialization
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+# Judge whether there is rgb input
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+# Decide resolutions for both depth and rgb streaming
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+profile = pipeline.start(config)
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+# Remove the background of objects more than clipping_distance_in_meters (user's set) meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
+
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+try:
+    with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while True:
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
+
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
+
+            # Get aligned frames
+            depth_frame = aligned_frames.get_depth_frame() # depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
+
+            # Validate that both frames are valid
+            if not depth_frame or not color_frame:
+                continue
+
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # # Remove background - Set pixels further than clipping_distance to grey
+            # grey_color = 153
+            # depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+            # bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+            # # Render images:
+            # #   depth align to color on left
+            # #   depth on right
+            # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            # images = np.hstack((bg_removed, depth_colormap))
+
+
+            results = hands.process(color_image)
+
+            color_image.flags.writeable = True
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        color_image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+
+                    x_index = math.floor(hand_landmarks.landmark[8].x * 640)
+                    y_index = math.floor(hand_landmarks.landmark[8].y * 480)
+                    cv2.circle(color_image, (x_index, y_index), 10, (0, 0, 255))
+                    # print(x_index, y_index)
+                    # print(hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y)
+                    finger_dis = depth_image[y_index][x_index]
+                    print(finger_dis)
+
+                # Flip the image horizontally for a selfie-view display.
+                cv2.imshow('MediaPipe Hands', cv2.flip(color_image, 1))
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+finally:
+    # Stop streaming
+    pipeline.stop()
+    cv2.destroyAllWindows()
