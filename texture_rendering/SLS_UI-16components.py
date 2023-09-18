e34af0022c298ab9b78fce09001ee5234860f605
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2023-09-18 21:08:10 +0900

    Added 16 frequency components SLS

---
+++ texture_rendering/SLS_UI-16components.py
@@ -0,0 +1,147 @@
+'''
+Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
+Date: 2023-06-01 16:46:22
+LastEditors: Mingxin Zhang
+LastEditTime: 2023-09-18 21:07:09
+Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
+'''
+import sys
+import numpy as np
+from PyQt5.QtWidgets import QApplication, QWidget, QSlider, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen, QPainterPath
+import math
+import pySequentialLineSearch
+
+FREQUENCY_LIST = [20, 35, 60, 100, 115,
+                  130, 145, 160, 185, 210, 
+                  235, 265, 300, 450, 670, 1000]
+
+# drawing the waveform
+class SinusoidWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setMinimumSize(400, 200)
+        self._frequency_gain = [1.0] * 16
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(palette)
+
+    def setGain(self, gain):
+        self._frequency_gain = gain
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(Qt.blue, 2))
+
+        width = self.width()
+        height = self.height()
+        x_scale = width / (0.3 * math.pi)
+        y_scale = height
+
+        path = QPainterPath()
+        path.moveTo(0, height / 2)
+
+        line_thickness = 1
+        painter.setPen(QPen(Qt.blue, line_thickness))
+        for x in range(width):
+            t = x / x_scale
+            y = 0
+            for i in range(16):
+                y += 0.5 * self._frequency_gain[i] * math.sin(FREQUENCY_LIST[i] * t) + 0.5
+            y = y / 16
+            path.lineTo(x, height - y * y_scale)
+        painter.drawPath(path)
+
+        # draw the axis
+        axis_thickness = 10
+        painter.setPen(QPen(Qt.black, axis_thickness))
+        painter.drawLine(0, height, width, height)
+        painter.drawLine(0, 0, 0, height)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.horizontal_slider = QSlider(Qt.Horizontal)
+        self.horizontal_slider.setRange(0, 999)
+        self.horizontal_slider.setSliderPosition(0)
+
+        self.vertical_sliders = []
+
+        self.sinusoid_widget = SinusoidWidget()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.sinusoid_widget)
+
+        horizontal_layout = QHBoxLayout()
+        labels = ["F_STM", "R", "20 Hz", "35 Hz", "60 Hz", "100 Hz", 
+                  "115 Hz", "130 Hz", "145 Hz", "160 Hz", "185 Hz", "210 Hz", 
+                  "235 Hz", "265 Hz", "300 Hz", "450 Hz", "670 Hz", "1000 Hz"]
+        for i in range(18):
+            vertical_slider = QSlider(Qt.Vertical)
+            vertical_slider.setRange(0, 100)
+            vertical_slider.setEnabled(False)
+            self.vertical_sliders.append(vertical_slider)
+
+            label = QLabel(labels[i])
+
+            vertical_box = QVBoxLayout()
+            vertical_box.addWidget(label, 1, Qt.AlignCenter | Qt.AlignTop)
+            vertical_box.addWidget(vertical_slider, 0, Qt.AlignCenter | Qt.AlignTop)
+
+            horizontal_layout.addLayout(vertical_box)
+
+        layout.addLayout(horizontal_layout)
+        layout.addWidget(self.horizontal_slider)
+
+        self.optimizer = pySequentialLineSearch.SequentialLineSearchOptimizer(num_dims=18)
+
+        self.optimizer.set_hyperparams(kernel_signal_var=0.50,
+                                kernel_length_scale=0.10,
+                                kernel_hyperparams_prior_var=0.10)
+        
+        self.optimizer.set_gaussian_process_upper_confidence_bound_hyperparam(5.)
+
+        self.horizontal_slider.valueChanged.connect(lambda value: 
+                                                    self.updateValues(_update_optimizer_flag=False))
+
+        next_button = QPushButton("Next")
+        next_button.clicked.connect(lambda value: self.updateValues(_update_optimizer_flag=True))
+        layout.addWidget(next_button)
+        self.setLayout(layout)
+
+        self.updateValues(_update_optimizer_flag=False)
+
+    def updateValues(self, _update_optimizer_flag):
+        slider_position = self.horizontal_slider.value() / 999.0
+
+        if _update_optimizer_flag:
+            self.optimizer.submit_feedback_data(slider_position)
+            print('Next')
+
+        optmized_para = self.optimizer.calc_point_from_slider_position(slider_position)
+
+        optmized_para[0] = 3 + optmized_para[0] * 17     # STM_freq: 3~20Hz
+        optmized_para[1] = 2 + optmized_para[1] * 3       # STM radius: 2~5mm
+        optmized_para[2:19] *= 4
+
+        # print('f_STM:', stm_freq, '\tradius: ', radius, '\tf_wave: ', freq, '\tamp: ', amp)
+        
+        self.sinusoid_widget.setGain(optmized_para[2:19])
+
+        i = 0
+        for vertical_slider in self.vertical_sliders:
+            vertical_slider.setValue(int(optmized_para[i] * vertical_slider.maximum()))
+            i += 1
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
